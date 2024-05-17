a87ea64fb8252c75427a4bd4dadd56a51eb09668
novakvova <novakvova@gmail.com>
2024-05-17 17:30:04 +0300

    Show Score counter

--- ApplePicker/Assets/Scripts/Basket.cs
+++ ApplePicker/Assets/Scripts/Basket.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text="0"; //початковий показник результату
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos2D = Input.mousePosition;
+
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+
+        if (collidedWith.CompareTag("Apple"))
+        {
+            Destroy(collidedWith);
+        }
     }
 }
