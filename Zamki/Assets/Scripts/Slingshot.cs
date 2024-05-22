7de05422b97ffcf45bd44ff24db20f7ecc22c695
novakvova <novakvova@gmail.com>
2024-05-22 20:42:27 +0300

    Begin working app

---
+++ Zamki/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+        print("On Mouse Enter");
+    }
+
+    private void OnMouseExit()
+    {
+        print("On Mouse Exit");
+    }
+}
